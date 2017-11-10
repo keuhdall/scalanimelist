37a2d36459dd18649444041c2462dbce136a450a
Léonard Marques <lmarques@student.42.fr>
2017-11-10 05:14:59 +0100

    XML parsing working

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,9 @@ import scala.xml.XML
 
 object Main {
   final val API_URL = "https://myanimelist.net/api/anime/search.xml?q="
-  var API_USER:String = null
-  var API_PASSWORD:String = null
+  final val URL_PREFIX = "https://myanimelist.net/anime/"
+  var API_USER:String = _
+  var API_PASSWORD:String = _
 
   def get(url: String, timeout : Int = 5000): String = {
     val connection = new URL(url).openConnection.asInstanceOf[HttpURLConnection]
@@ -32,7 +33,21 @@ object Main {
     arg = arg.replaceAll(" ", "+")
     try {
       val xmlElem = XML.loadString(get(API_URL + arg))
-      println(get(API_URL + arg))
+      val nbResults = (xmlElem \\ "anime" \\ "entry").length
+      if (nbResults > 1)
+        println("\n => " + nbResults + " Results found :\n" + "\n=======================================")
+      else
+        println("\n => " + nbResults + " Result found :\n" + "\n=======================================")
+
+      (xmlElem \\ "anime" \\ "entry").map(_.foreach(e => {
+        print("Title : " + (e \\ "title").text + '\n' +
+          "Episodes : " + (e \\ "episodes").text + '\n' +
+          "Score : " + (e \\ "score").text + '\n' +
+          "Type : " + (e \\ "type").text + '\n' +
+          "Status : " + (e \\ "status").text + '\n' +
+          "Link : " + URL_PREFIX + (e \\ "id").text + '\n')
+        println("\n=======================================\n")
+      }))
     } catch {
       case ioe: java.io.IOException => ioe.printStackTrace()
       case ste: java.net.SocketTimeoutException => ste.printStackTrace()
